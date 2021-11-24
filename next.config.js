d3d9127720c6943e0df400a01cb8c30684529a7a
peteranwar <peteranwar1997@gmail.com>
2021-11-24 14:01:55 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
+  images: {
+    domains: ['wawinner.its.ae']
+  },
   reactStrictMode: true,
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import '../src/scss/style.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
